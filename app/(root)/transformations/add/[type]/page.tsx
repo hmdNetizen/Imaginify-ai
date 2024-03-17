ae7394374aa5da565b32eccb99f576d74403e290
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2024-03-17 15:53:37 +0100

    Adjusts the mongoose file

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,33 @@
+import Header from "@/components/shared/Header";
 import React from "react";
+import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-function AddTransformationsTypePage() {
-  return <div>AddTransformationsTypePage</div>;
+async function AddTransformationsTypePage({
+  params: { type },
+}: SearchParamProps) {
+  const transformation = transformationTypes[type];
+
+  const { userId } = auth();
+
+  if (!userId) return redirect("/sign-in");
+
+  const user = await getUserById(userId);
+
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <TransformationForm
+        action="Add"
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
+    </>
+  );
 }
 
 export default AddTransformationsTypePage;
